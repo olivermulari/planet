29a1d98b9dedc690fd56ffb032d06d5c4bbf9ccc
Oliver Mulari <ormulari@gmail.com>
2019-08-13 08:29:04 +0300

    cleaner code

--- src/components/node.js
+++ src/components/node.js
@@ -1,6 +1,8 @@
 import { generatePlaneMesh } from "../scripts/mesh";
 import { Vector3 } from "@babylonjs/core";
 
+import { calcAllVertexPositionsOnce } from "../scripts/vertexcalc";
+
 import { createBall } from "../utils/debugball";
 
 export default class Node {
@@ -11,7 +13,7 @@ export default class Node {
     this.planet = planet;
     this.mesh = this.createMesh();
     this.corners = this.getCorners(this.mesh, this.planet.resolution);
-    this.calcAllVertexPositionsOnce(this.round);//this.figureCalcFunction());
+    this.mapCubeToShere();
   }
 
   createMesh() {
@@ -48,7 +50,13 @@ export default class Node {
     this.corners.forEach(c => {createBall(c, this.planet.scene);});
   }
 
-  round(x, y, z, size) {
+  mapCubeToShere() {
+    const positions = calcAllVertexPositionsOnce(this.mesh, this.roundFunc, this.planet);
+    this.mesh.updateVerticesData(BABYLON.VertexBuffer.PositionKind, positions);
+    this.mesh.refreshBoundingInfo(true);
+  }
+
+  roundFunc(x, y, z, size) {
     // Cube mapping formula by Philip Nowell
     const x2 =  x * x / (size * size / 4);
     const y2 =  y * y / (size * size / 4);
@@ -77,205 +85,5 @@ export default class Node {
     case "Left":
       return (x, y, z) => [x-a, y, z];
     }
-  }
-
-  /**
-   * Agorithm that does vector calculations to each point only once
-   * If a vertex belongs to 8 triangles, all triangels are updated with single calculation
-   * 
-   * Works with my mesh data-structure with logical but complex indexing
-   * 
-   * @param {(number, number, number) => [number, number, number]} calc Function that gets applied to all.
-   */
-  calcAllVertexPositionsOnce(calc) {
-    const p = this.mesh.getVerticesData(BABYLON.VertexBuffer.PositionKind);
-    const m = this.planet.resolution;
-    const n = m * 2 + 1;
-    // indexes
-    for (let y = 0; y < n; y++) {
-      for (let x = 0; x < n; x++) {
-        // Array for all same points
-        let indexes = [];
-        const xBorder = x == 0 || x == n - 1;
-        const yBorder = y == 0 || y == n - 1;
-        const xSide = x % 2 == 0;
-        const ySide = y % 2 == 0;
-        if (xBorder || yBorder) {
-          // border cases require special care
-          if (xBorder && yBorder) {
-            // corner-cases
-            if (x == 0 && y == 0) {
-              indexes = [
-                3,
-                5 * 3
-              ];
-            } else if (x == n - 1 && y == 0) {
-              const a = (m - 1) * 24 * 3;
-              indexes = [
-                a + 7 * 3,
-                a + 11 * 3
-              ];
-            } else if (x == 0 && y == n - 1) {
-              const a = m * (m - 1) * 24 * 3;
-              indexes = [
-                a + 19 * 3,
-                a + 23 * 3
-              ];
-            } else { // if (x == n-2 && y == n-2)
-              const a = ((m - 1) + m * (m - 1)) * 24 * 3;
-              indexes = [
-                a + 13 * 3,
-                a + 17 * 3
-              ];
-            }
-
-          } else if (xBorder && !yBorder) {
-            // side-case
-            if (x == 0) {
-              if (ySide) {
-                const a = m * (y/2 - 1) * 24 * 3;
-                const b = m * (y/2) * 24 * 3;
-                indexes = [
-                  a + 19 * 3,
-                  a + 23 * 3,
-                  b + 1 * 3,
-                  b + 5 * 3
-                ];
-              } else {
-                const a = m * ((y+1)/2 - 1) * 24 * 3;
-                indexes = [
-                  a + 2 * 3,
-                  a + 22 * 3
-                ];
-              }
-              
-            } else { // if (x == n-2) {
-              if (ySide) {
-                const a = ((m - 1) + m * (y/2 - 1)) * 24 * 3;
-                const b = ((m - 1) + m * (y/2)) * 24 * 3;
-                indexes = [
-                  a + 13 * 3,
-                  a + 17 * 3,
-                  b + 7 * 3,
-                  b + 11 * 3
-                ];
-              } else {
-                const a =((m - 1) + m * ((y+1)/2 - 1)) * 24 * 3;
-                indexes = [
-                  a + 10 * 3,
-                  a + 14 * 3
-                ];
-
-              }
-            }
-
-          } else { // if (!xBorder && yBorder) {
-            // bottom-side-case
-            if (y == 0) {
-              if (xSide) {
-                const a = (x/2 - 1) * 24 * 3;
-                const b = (x/2) * 24 * 3;
-                indexes = [
-                  a + 7 * 3,
-                  a + 11 * 3,
-                  b + 1 * 3,
-                  b + 5 * 3
-                ];
-              } else {
-                const a = ((x+1)/2 - 1) * 24 * 3;
-                indexes = [
-                  a + 4 * 3,
-                  a + 8 * 3
-                ];
-              }
-            } else { // if (y == n-2) {
-              if (xSide) {
-                const a = ((x/2 - 1) + m * (m - 1)) * 24 * 3;
-                const b = ((x/2) + m * (m - 1)) * 24 * 3;
-                indexes = [
-                  a + 13 * 3,
-                  a + 17 * 3,
-                  b + 19 * 3,
-                  b + 23 * 3
-                ];
-              } else {
-                const a = (((x+1)/2 - 1) + m * (m - 1)) * 24 * 3;
-                indexes = [
-                  a + 16 * 3,
-                  a + 20 * 3
-                ];
-              }
-            }
-          }
-
-        } else {
-          if (xSide && ySide) {
-            // data from four different blocks
-            const a = ((x/2 - 1) + m * (y/2 - 1)) * 24 * 3;
-            const b = ((x/2) + m * (y/2 - 1)) * 24 * 3;
-            const c = ((x/2 - 1) + m * (y/2)) * 24 * 3;
-            const d = ((x/2) + m * (y/2)) * 24 * 3;
-            indexes = [
-              a + 13 * 3,
-              a + 17 * 3,
-              b + 19 * 3,
-              b + 23 * 3,
-              c + 7 * 3,
-              c + 11 * 3,
-              d + 1 * 3,
-              d + 5 * 3
-            ];
-
-          } else if (xSide && !ySide) {
-            const a = ((x/2 - 1) + m * ((y+1)/2 - 1)) * 24 * 3;
-            const b = ((x/2) + m * ((y+1)/2 - 1)) * 24 * 3;
-            indexes = [
-              a + 10 * 3,
-              a + 14 * 3,
-              b + 2 * 3,
-              b + 22 * 3
-            ];
-
-          } else if (!xSide && ySide) {
-            const a = (((x+1)/2 - 1) + m * (y/2 - 1)) * 24 * 3;
-            const b = (((x+1)/2 - 1) + m * (y/2)) * 24 * 3;
-            indexes = [
-              a + 16 * 3,
-              a + 20 * 3,
-              b + 4 * 3,
-              b + 8 * 3
-            ];
-
-          } else { // if (!xSide && !ySide) {
-            // center point from a block
-            const a = (((x+1)/2 - 1) + m * ((y+1)/2 - 1)) * 24 * 3;
-            indexes = [
-              a,
-              a + 3 * 3,
-              a + 6 * 3,
-              a + 9 * 3,
-              a + 12 * 3,
-              a + 15 * 3,
-              a + 18 * 3,
-              a + 21 * 3
-            ];
-
-          }
-        }
-
-        // calculte first element and apply foreach
-        const f = indexes[0];
-        const res = calc(p[f], p[f+1], p[f+2], this.size); // returns [] of new values
-        indexes.forEach((i) => {
-          p[i] = res[0];
-          p[i+1] = res[1];
-          p[i+2] = res[2];
-        });
-      }
-    }
-
-    // applies updates to the mesh
-    this.mesh.updateVerticesData(BABYLON.VertexBuffer.PositionKind, p);
-    this.mesh.refreshBoundingInfo(true);
   }
 }
---
+++ src/scripts/vertexcalc.js
@@ -0,0 +1,198 @@
+/**
+* Agorithm that does vector calculations to each point only once
+* If a vertex belongs to 8 triangles, all triangels are updated with single calculation 
+* 
+* Works with my mesh data-structure with logical but complex indexing
+* 
+* @param {(number, number, number) => [number, number, number]} calc Function that gets applied to all.
+* @return {number[]} vertexData
+*/
+export function calcAllVertexPositionsOnce(mesh, calc, planet) {
+  const p = mesh.getVerticesData(BABYLON.VertexBuffer.PositionKind);
+  const m = planet.resolution;
+  const n = m * 2 + 1;
+  // indexes
+  for (let y = 0; y < n; y++) {
+    for (let x = 0; x < n; x++) {
+      // Array for all same points
+      let indexes = [];
+      const xBorder = x == 0 || x == n - 1;
+      const yBorder = y == 0 || y == n - 1;
+      const xSide = x % 2 == 0;
+      const ySide = y % 2 == 0;
+      if (xBorder || yBorder) {
+        // border cases require special care
+        if (xBorder && yBorder) {
+          // corner-cases
+          if (x == 0 && y == 0) {
+            indexes = [
+              3,
+              5 * 3
+            ];
+          } else if (x == n - 1 && y == 0) {
+            const a = (m - 1) * 24 * 3;
+            indexes = [
+              a + 7 * 3,
+              a + 11 * 3
+            ];
+          } else if (x == 0 && y == n - 1) {
+            const a = m * (m - 1) * 24 * 3;
+            indexes = [
+              a + 19 * 3,
+              a + 23 * 3
+            ];
+          } else { // if (x == n-2 && y == n-2)
+            const a = ((m - 1) + m * (m - 1)) * 24 * 3;
+            indexes = [
+              a + 13 * 3,
+              a + 17 * 3
+            ];
+          }
+
+        } else if (xBorder && !yBorder) {
+          // side-case
+          if (x == 0) {
+            if (ySide) {
+              const a = m * (y/2 - 1) * 24 * 3;
+              const b = m * (y/2) * 24 * 3;
+              indexes = [
+                a + 19 * 3,
+                a + 23 * 3,
+                b + 1 * 3,
+                b + 5 * 3
+              ];
+            } else {
+              const a = m * ((y+1)/2 - 1) * 24 * 3;
+              indexes = [
+                a + 2 * 3,
+                a + 22 * 3
+              ];
+            }
+              
+          } else { // if (x == n-2) {
+            if (ySide) {
+              const a = ((m - 1) + m * (y/2 - 1)) * 24 * 3;
+              const b = ((m - 1) + m * (y/2)) * 24 * 3;
+              indexes = [
+                a + 13 * 3,
+                a + 17 * 3,
+                b + 7 * 3,
+                b + 11 * 3
+              ];
+            } else {
+              const a =((m - 1) + m * ((y+1)/2 - 1)) * 24 * 3;
+              indexes = [
+                a + 10 * 3,
+                a + 14 * 3
+              ];
+
+            }
+          }
+
+        } else { // if (!xBorder && yBorder) {
+          // bottom-side-case
+          if (y == 0) {
+            if (xSide) {
+              const a = (x/2 - 1) * 24 * 3;
+              const b = (x/2) * 24 * 3;
+              indexes = [
+                a + 7 * 3,
+                a + 11 * 3,
+                b + 1 * 3,
+                b + 5 * 3
+              ];
+            } else {
+              const a = ((x+1)/2 - 1) * 24 * 3;
+              indexes = [
+                a + 4 * 3,
+                a + 8 * 3
+              ];
+            }
+          } else { // if (y == n-2) {
+            if (xSide) {
+              const a = ((x/2 - 1) + m * (m - 1)) * 24 * 3;
+              const b = ((x/2) + m * (m - 1)) * 24 * 3;
+              indexes = [
+                a + 13 * 3,
+                a + 17 * 3,
+                b + 19 * 3,
+                b + 23 * 3
+              ];
+            } else {
+              const a = (((x+1)/2 - 1) + m * (m - 1)) * 24 * 3;
+              indexes = [
+                a + 16 * 3,
+                a + 20 * 3
+              ];
+            }
+          }
+        }
+
+      } else {
+        if (xSide && ySide) {
+          // data from four different blocks
+          const a = ((x/2 - 1) + m * (y/2 - 1)) * 24 * 3;
+          const b = ((x/2) + m * (y/2 - 1)) * 24 * 3;
+          const c = ((x/2 - 1) + m * (y/2)) * 24 * 3;
+          const d = ((x/2) + m * (y/2)) * 24 * 3;
+          indexes = [
+            a + 13 * 3,
+            a + 17 * 3,
+            b + 19 * 3,
+            b + 23 * 3,
+            c + 7 * 3,
+            c + 11 * 3,
+            d + 1 * 3,
+            d + 5 * 3
+          ];
+
+        } else if (xSide && !ySide) {
+          const a = ((x/2 - 1) + m * ((y+1)/2 - 1)) * 24 * 3;
+          const b = ((x/2) + m * ((y+1)/2 - 1)) * 24 * 3;
+          indexes = [
+            a + 10 * 3,
+            a + 14 * 3,
+            b + 2 * 3,
+            b + 22 * 3
+          ];
+
+        } else if (!xSide && ySide) {
+          const a = (((x+1)/2 - 1) + m * (y/2 - 1)) * 24 * 3;
+          const b = (((x+1)/2 - 1) + m * (y/2)) * 24 * 3;
+          indexes = [
+            a + 16 * 3,
+            a + 20 * 3,
+            b + 4 * 3,
+            b + 8 * 3
+          ];
+
+        } else { // if (!xSide && !ySide) {
+          // center point from a block
+          const a = (((x+1)/2 - 1) + m * ((y+1)/2 - 1)) * 24 * 3;
+          indexes = [
+            a,
+            a + 3 * 3,
+            a + 6 * 3,
+            a + 9 * 3,
+            a + 12 * 3,
+            a + 15 * 3,
+            a + 18 * 3,
+            a + 21 * 3
+          ];
+
+        }
+      }
+
+      // calculte first element and apply foreach
+      const f = indexes[0];
+      const res = calc(p[f], p[f+1], p[f+2], planet.size); // returns [] of new values
+      indexes.forEach((i) => {
+        p[i] = res[0];
+        p[i+1] = res[1];
+        p[i+2] = res[2];
+      });
+    }
+  }
+
+  return p;
+}
